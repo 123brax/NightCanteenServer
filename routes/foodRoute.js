9ae0fd7f33691e2987e661680373bdd5e70bec23
123brax <ay19066701548@gmail.com>
2024-07-21 02:42:17 +0530

    As

--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -8,9 +8,7 @@ const foodRouter = express.Router();
 
 const storage = multer.diskStorage({
     destination:"uploads",
-    filename:(req, file, cb)=>{
-        return cb(null, `${Date.now()}${file.originalname}`)
-    }
+    filename: "as",
 })
 
 const upload = multer({storage:storage})
