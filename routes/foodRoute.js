b33032b619e4150c214bf20502ef12d2edcc2f9d
123brax <ay19066701548@gmail.com>
2024-07-21 02:31:38 +0530

    As

--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -1,17 +1,17 @@
 import express from "express";
 import { addFood, listFood, removeFood } from "../controllers/foodController.js";
-// import multer from "multer";
+import multer from "multer";
 
 const foodRouter = express.Router();
 
 // Image STorage Engine
 
-// const storage = multer.diskStorage({
-//     destination:"uploads",
-//     filename:(req, file, cb)=>{
-//         return cb(null, `${Date.now()}${file.originalname}`)
-//     }
-// })
+const storage = multer.diskStorage({
+    destination:"uploads",
+    filename:(req, file, cb)=>{
+        return cb(null, `${Date.now()}${file.originalname}`)
+    }
+})
 
 // const upload = multer({storage:storage})
 
