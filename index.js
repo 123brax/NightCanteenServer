3a72063263a0a1d05821a06ac16f4c62b9b06171
123brax <ay19066701548@gmail.com>
2024-07-21 02:26:51 +0530

    As

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { connectDB } from "./config/db.js";
 import userRouter from "./routes/userRoute.js";
 import 'dotenv/config.js'
 import cartRouter from "./routes/cartRoute.js";
-// import orderRouter from "./routes/orderRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 
 
 // app config
@@ -23,7 +23,7 @@ connectDB()
 // app.use("/api/food", foodRouter)
 app.use("/api/user", userRouter)
 app.use("/api/cart", cartRouter)
-// app.use("/api/order", orderRouter)
+app.use("/api/order", orderRouter)
 app.use("/images", express.static('uploads'))
 
 app.get("/",(req, res)=>{
