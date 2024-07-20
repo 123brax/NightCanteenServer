494b8b9d75dfbfc0c8f19b2690d3b6f7dcb344bc
123brax <ay19066701548@gmail.com>
2024-07-21 02:22:51 +0530

    As

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors"
 import { connectDB } from "./config/db.js";
-import foodRouter from "./routes/foodRoute.js";
-// import userRouter from "./routes/userRoute.js";
+// import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
 import 'dotenv/config.js'
 // import cartRouter from "./routes/cartRoute.js";
 // import orderRouter from "./routes/orderRoute.js";
@@ -20,8 +20,8 @@ app.use(cors())
 connectDB()
 
 // api endpoint
-app.use("/api/food", foodRouter)
-// app.use("/api/user", userRouter)
+// app.use("/api/food", foodRouter)
+app.use("/api/user", userRouter)
 // app.use("/api/cart", cartRouter)
 // app.use("/api/order", orderRouter)
 app.use("/images", express.static('uploads'))
